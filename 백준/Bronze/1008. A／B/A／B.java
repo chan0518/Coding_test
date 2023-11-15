10b231002d52acf5502f48a7150802d107369da6
Park Chan <114585572+chan0518@users.noreply.github.com>
2023-11-15 14:54:10 +0900

    [Bronze V] Title: A/B, Time: 228 ms, Memory: 17916 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        Double a = sc.nextDouble();
+        Double b = sc.nextDouble();
+        System.out.println(a/b);
+    }
+}
