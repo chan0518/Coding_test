c164312a433ca9bbf523ea1dad572ff3ddb420df
WhitePengdori <114585572+chan0518@users.noreply.github.com>
2023-12-10 20:52:16 +0900

    [Bronze III] Title: ACM 호텔, Time: 256 ms, Memory: 18140 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int i=0;i<T;i+=1){
+            int H = sc.nextInt();
+            int W = sc.nextInt();
+            int N = sc.nextInt();
+            int a = N/H;
+            int b =  N%H==0 ? H*100:(N%H) * 100+1;
+
+            System.out.println(a+b);
+        }
+    }
+}
