cce3ad802d54b6349679e9757b3b2171d7ee3086
WhitePengdori <114585572+chan0518@users.noreply.github.com>
2024-01-29 19:57:29 +0900

    [Bronze III] Title: 최댓값, Time: 236 ms, Memory: 18520 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a[][] = new int[9][9];
+
+        int max = 0, x=0, y=0;
+        for (int i=0; i<a.length; i++){
+            for (int j=0; j<a[0].length; j++){
+                a[i][j] = sc.nextInt();
+                if (max < a[i][j]){
+                    max = a[i][j];
+                    x = j;
+                    y = i;
+                }
+            }
+        }
+        System.out.println(max);
+        System.out.println(++y +" "+ ++x);
+    }
+}
