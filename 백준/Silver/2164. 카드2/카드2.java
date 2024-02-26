715759dfad563725e3355963f6f70185c645c132
WhitePengdori <114585572+chan0518@users.noreply.github.com>
2024-02-27 03:36:45 +0900

    [Silver IV] Title: 카드2, Time: 140 ms, Memory: 18288 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,33 +1,25 @@
-import java.util.Scanner;
- 
+import java.io.*;
+
 public class Main {
- 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		
-		int N = in.nextInt();
-		
-		/**
-		 *  한 턴에 1개 씩 삭제되고 뒤에 1개가 추가 되므로
-		 *  2 * N - 1 의 공간이 필요하다.
-		 *  다만 index는 1부터 시작할 것이기 때문에
-		 *  2 * N 공간으로 생성한다.   
-		 */
-		int[] q = new int[2 * N];	  
-		for(int i = 1; i <= N; i++) {
-			q[i] = i;
-		}
-		int prev_index = 1;
-		int last_index = N;
-		
-		while(N-- > 1) {
-			prev_index++;
-			q[last_index + 1] = q[prev_index];
-			last_index++;
-			prev_index++;
-		}
- 
-		System.out.println(q[prev_index]);
-	}
- 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.valueOf(br.readLine());
+
+        int arr[] = new int[N*2];
+        for(int i = 1; i <= N; i++) {
+            arr[i] = i;
+        }
+        int idx = 0;
+        int add = N;
+
+        while(N-- > 1) {
+            idx += 2;
+            arr[add + 1] = arr[idx];
+            add++;
+        }
+        bw.write(String.valueOf(arr[add]));
+        bw.close();
+    }
 }
