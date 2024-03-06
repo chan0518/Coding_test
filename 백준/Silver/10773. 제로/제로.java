78f97f8025e1caf7340390f3b4f8ad7311879724
WhitePengdori <114585572+chan0518@users.noreply.github.com>
2024-03-06 12:02:14 +0900

    [Silver IV] Title: 제로, Time: 228 ms, Memory: 22700 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int K = Integer.valueOf(br.readLine());
+        int arr[] = new int[K+1];
+
+        int idx = -1;
+        for (int i=0; i<K; i++){
+            int num = Integer.valueOf(br.readLine());//숫자를 받는다
+            if (num == 0){//받은 숫자가 0이라면 최근 인덱스의 값을 0으로 만들고 인덱스를 1감소한다
+                arr[idx--] = 0;
+            }else {//받은 숫자가 0이 아니라면 인덱스를 1증가 시키고 값을 넣는다
+                arr[++idx] = num;
+            }
+        }
+
+        int sum = 0;
+        int i = 0;
+        while (arr[i] != 0){//arr값이 0이 아니라면 반복문 실행
+            sum += arr[i];//sum에다가 arr에 있는 숫자값들의 합을 저장
+            i++;
+        }
+
+        bw.write(String.valueOf(sum));
+        bw.close();
+    }
+}
