bb0b5073ef8933351ea17cf33b323e9cb6dc72b2
WhitePengdori <114585572+chan0518@users.noreply.github.com>
2024-04-20 17:31:53 +0900

    [Bronze II] Title: Hashing, Time: 132 ms, Memory: 14280 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -6,13 +6,16 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        final int M = 1234567891;
         long result = 0;
         int N = Integer.valueOf(br.readLine());
         String[] str = br.readLine().split("");
+        long pow = 1;
         for (int i=0; i<N; i++){
-            long alpha = (long) str[i].charAt(0) - 96;
-            result += alpha * (long) Math.pow(31, i);
+            long alpha = (long) str[i].charAt(0) - 96 % M;
+            result += alpha * pow % M;
+            pow = pow * 31 % M;
         }
-        System.out.println(result % 1234567891);
+        System.out.println(result % M);
     }
 }
