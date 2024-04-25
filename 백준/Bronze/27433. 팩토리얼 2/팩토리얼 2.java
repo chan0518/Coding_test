faf62bb804aaaeff8a5adac058f0fed47ad4bb5b
WhitePengdori <114585572+chan0518@users.noreply.github.com>
2024-04-25 17:58:24 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 124 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -1,17 +1,23 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        long N = Long.valueOf(br.readLine());
-        long num = 1;
-        for (long i=N; i>1; i--){
-            num *= i;
-        }
-        System.out.println(num);
+	public static void main(String[] args) throws Exception {
 
-    }
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+
+		if (N < 0 || N > 20)
+			return;
+
+		System.out.println(getFactorial(N));
+
+	}
+
+	public static long getFactorial(int num) {
+
+		return num == 0 || num == 1 ? 1 : num * getFactorial(num - 1);
+	}
 }
