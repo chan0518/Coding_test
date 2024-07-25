b04c934ef0d3964cbb2414ece57a670db0319c52
Chan_E <114585572+chan0518@users.noreply.github.com>
2024-07-25 20:39:41 +0900

    [Silver V] Title: 단어 나누기, Time: 180 ms, Memory: 19976 KB -BaekjoonHub

--- 백준/Silver/1251. 단어 나누기/단어 나누기.java
+++ 백준/Silver/1251. 단어 나누기/단어 나누기.java
@@ -20,28 +20,16 @@ public class Main {
 				
 				StringBuilder sb[] = new StringBuilder[3];
 				for(int k=0; k<3; k++) sb[k] = new StringBuilder();//3개의 단어를 만들어야 하므로 스트링빌더 3개 만듦
-				for(int c=0; c<=i; c++) sb[0].append(str[c]);
-				for(int c=i+1; c<=j; c++) sb[1].append(str[c]);
-				for(int c=j+1; c<=len-1; c++) sb[2].append(str[c]);
-				String reverse[] = new String[3];
-				
+				for(int c=0; c<=i; c++) sb[0].append(str[c]);//0부터 i까지 단어 1번
+				for(int c=i+1; c<=j; c++) sb[1].append(str[c]);//i+1부터 	j까지 단어 2번
+				for(int c=j+1; c<len; c++) sb[2].append(str[c]);//j+1부터 끝까지 단어 3번
 				StringBuilder SB = new StringBuilder();
-				for(int q=0; q<3; q++) SB.append(sb[q].reverse().toString());//리벌스 스트링 완
+				for(int q=0; q<3; q++) SB.append(sb[q].reverse().toString());//리버스한 단어3개들을 합침
 				
 				result = SB.toString().compareTo(result) < 0 ? SB.toString() : result;
+				//SB.toString()을 기준으로 비교 했을 때 음수가 나오면 사전순으로 빠르기 때문에 음수가 나오면 result = SB.toString()을 해
 			}
 		}
-		return result;
+		return result;//가장 사전순으로 빠른단어를 리
 	}
 }
-
-/*
-1 2 3 4 5 6 7 8
-6 5 4 3 2 1
-21
-
-
-1 /2 /3 4 5
-3 2 1
-
-*/
