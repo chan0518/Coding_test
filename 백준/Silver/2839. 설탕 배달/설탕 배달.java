f5f71b573fdd9c1eba3c52a74727bb08ccf4d75e
Chan_E <114585572+chan0518@users.noreply.github.com>
2024-08-20 17:47:57 +0900

    [Silver IV] Title: 설탕 배달, Time: 164 ms, Memory: 17724 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -8,25 +8,19 @@ public class Main {
         //6, 2
         int N = sc.nextInt();//입력
         int a = N/5;
-        int b=0,c=0;
-        boolean q = false;
+        int b = 0, c = 0;
+        boolean TF = false;
 
-        for(;a>0;a-=1){
+        for(;a>0;a--){
             b = N-(5*a);
             if (b%3 == 0) {
                 c = b / 3;
-                q = true;
+                TF = true;
                 break;
             }
         }
-        if (q){
-            System.out.println(c + a);
-        }
-        else if (N%3 == 0){
-            System.out.println(N/3);
-        }
-        else {
-            System.out.println(-1);
-        }
+        if (TF)System.out.println(c + a);
+        else if (N%3 == 0) System.out.println(N/3);
+        else System.out.println(-1);
     }
 }
