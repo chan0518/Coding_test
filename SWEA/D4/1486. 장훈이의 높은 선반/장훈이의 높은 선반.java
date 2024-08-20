6cb36b257352745057ed670c4dbf63681aeb2985
Chan_E <114585572+chan0518@users.noreply.github.com>
2024-08-20 16:25:24 +0900

    [D4] Title: 장훈이의 높은 선반, Time: 188 ms, Memory: 35,572 KB -BaekjoonHub

---
+++ SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+ 
+class Solution {
+    static StringBuilder sb = new StringBuilder();
+    static int T, N, B;
+    static int[] arr;
+    static boolean[] visited;
+    static int result;
+    static ArrayList<Integer> li;
+    
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+ 
+        T = Integer.valueOf(br.readLine());
+         
+        for (int tc = 1; tc <= T; tc++) {
+        	//////////////////////////////////////////////////////////////////////////////////
+            sb.append("#").append(tc).append(" ");
+             
+            st = new StringTokenizer(br.readLine());
+            N = Integer.valueOf(st.nextToken());
+            B = Integer.valueOf(st.nextToken());
+            arr = new int[N];
+            visited = new boolean[N];
+            li = new ArrayList<>();
+            result = Integer.MAX_VALUE;
+            
+            st = new StringTokenizer(br.readLine());
+            for(int i=0; i<N; i++) arr[i] = Integer.parseInt(st.nextToken());
+            // 입력 완료
+            
+            dfs(0);
+            
+ 
+            sb.append(result - B).append("\n");
+            
+            //////////////////////////////////////////////////////////////////////////////////
+        }
+        System.out.println(sb.toString());
+    }
+    
+    static int liSum() {
+    	int sum = 0;
+    	for(int e : li) sum += e;
+    	return sum;
+    }
+    
+    static void dfs(int start) {
+    	int liSum = liSum();
+    	if(liSum >= B) {
+    		result = Math.min(result, liSum);
+    		return;
+    	}
+    	for(int i=start; i<N; i++) {
+    		if(visited[i]) continue;
+    		visited[i] = true;
+    		li.add(arr[i]);
+    		dfs(i+1);
+    		visited[i] = false;
+    		li.remove(li.size()-1);
+    	}
+    }
+    
+}
