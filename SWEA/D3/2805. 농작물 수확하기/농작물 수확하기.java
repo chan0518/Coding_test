bd1b67025d6051c2aea6079b8b7ac3c53637544b
Chan_E <114585572+chan0518@users.noreply.github.com>
2024-08-21 09:51:09 +0900

    [D3] Title: 농작물 수확하기, Time: 203 ms, Memory: 42,296 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
@@ -1,49 +1,75 @@
-
-import java.util.*;
-import java.io.FileInputStream;
-
-class Solution{
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+ 
+class Solution {
+    static StringBuilder sb = new StringBuilder();
+    static int T, N;
+    static int[][] map;
+    static boolean[][] visited;
+    static int result;
+    static int[][] D = {
+    		{-1, 0},	// 상
+    		{1, 0},		// 하
+    		{0, -1},	// 좌
+    		{0, 1},		// 우
+    };
+    
     public static void main(String args[]) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-
-        for (int test_case = 1; test_case <= T; test_case++) {
-            //여기 사이에 코드를 넣을 것
-
-            int n = sc.nextInt();
-            int sum = 0;
-            int[][] arr = new int[n][n];
-            for (int i=0; i<n; i++){
-                String[] str = sc.next().split("");
-                for (int j=0; j<n; j++) arr[i][j] = Integer.valueOf(str[j]);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+        T = Integer.valueOf(br.readLine());
+        // T 입력 받음
+         
+        for (int tc = 1; tc <= T; tc++) {
+        	sb.append("#").append(tc).append(" ");
+        	//////////////////////////////////////////////////////////////////////////////////
+        	N = Integer.parseInt(br.readLine());
+        	result = 0;
+        	
+            map = new int[N][N];
+            visited = new boolean[N][N];
+            
+            for(int i=0; i<N; i++) {
+            	char[] st = br.readLine().toCharArray();
+            	for(int j=0; j<N; j++) map[i][j] = st[j] - '0';
             }
-
-            boolean size = false;
-            int idx=n/2;
-            for (int i=0; i<n; i++){
-                if (!size){
-                    for (int j=n/2 - i; j<= n/2 + i; j++){
-                        sum += arr[i][j];
-                    }
-                    if (n/2-i == 0) size = true;
-                }
-                else {
-                    idx--;
-                    for (int j=n/2-idx; j<= n/2+idx; j++) {
-                        sum += arr[i][j];
-                    }
-                }
-            }
-
-            System.out.printf("#%d %d\n",test_case, sum);
-
-//2차원배열 출력
-//            for (int[] e1: arr){
-//                for (int e2: e1) System.out.print(e2 + " ");
-//                System.out.println();
-//            }
-
-            //여기사이에 코드를 넣을 것
+            
+            if(N==1) result = map[0][0];
+            else bfs(0);
+            
+            
+            //////////////////////////////////////////////////////////////////////////////////
+            sb.append(result).append("\n");
         }
+        System.out.println(sb.toString());
+    }
+    
+    static void bfs(int num) {
+    	ArrayDeque<int[]> q = new ArrayDeque<>();
+    	q.add(new int[] {N/2,N/2,num}); // (x, y)와 뎁스 0을 넘겨줌
+    	visited[N/2][N/2] = true;//정중앙은 탐색 했으니 true;
+    	
+    	while(!q.isEmpty()) {
+    		int[] current = q.poll();
+    		int x = current[0];// 현재 x값
+    		int y = current[1];// 현재 y값
+    		int depth = current[2]; // 현재 뎁스
+    		result += map[x][y];
+    		
+    		for(int[] d : D) {
+    			int nx = x + d[0]; // 탐색 할 x값
+    			int ny = y + d[1]; // 탐색 할 y값
+    			int nd = depth+1;
+    			
+    			if(0 <= nx && nx < N && 0 <= ny && ny < N && !visited[nx][ny] && nd <= N/2) {
+    				// 탐색할 x,y 값인 nx,ny가 map범위 안에 있고 방문을 안했고 탐색 할 위치의 뎁스가 N/2 이하라면
+    				q.add(new int[] {nx,ny, nd});
+    				visited[nx][ny] = true;
+    			}
+    		}
+    	}
     }
 }
