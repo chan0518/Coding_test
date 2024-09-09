6603b0139ecbc9f28329075bfc2d12534290a39e
Chan_E <114585572+chan0518@users.noreply.github.com>
2024-09-10 01:36:26 +0900

    [Gold IV] Title: 알고스팟, Time: 88 ms, Memory: 12408 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.java
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
@@ -34,7 +35,6 @@ public class Main {
  
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
  
         M = Integer.parseInt(st.nextToken()); // 가로
