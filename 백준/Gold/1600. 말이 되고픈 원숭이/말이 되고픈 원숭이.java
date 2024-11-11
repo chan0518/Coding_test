733dd0c11c7d749ea9d61d38cc208f8c77a662b5
Chan_E <114585572+chan0518@users.noreply.github.com>
2024-11-11 21:23:59 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 432 ms, Memory: 60424 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
@@ -1,97 +1,105 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int K, W, H;
-    static int[][] map;
-    static boolean[][][] visited; // 3차원 배열로 방문 상태를 기록
-    static int[][] D = {
-            {-1, 0}, // 상
-            {1, 0},  // 하
-            {0, -1}, // 좌
-            {0, 1},  // 우
-    };
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static int K, W, H;
+	static int[][] map;
+	static boolean[][][] visited;
+	static int[][] D = {
+			{-1, 0},	// 상
+			{1, 0},		// 하
+			{0, -1},	// 좌
+			{0, 1},		// 우
+	};
+	
+	static int[][] Horse = {
+			{-1,-2},
+			{-2,-1},
+			{-2, 1},
+			{-1, 2},
+			{1, 2},
+			{2, 1},
+			{2, -1},
+			{1, -2},
+	};
+	
+	
+	public static void main(String[] args) throws IOException {
+		K = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		W = Integer.parseInt(st.nextToken());
+		H = Integer.parseInt(st.nextToken());
+		
+		map = new int[H][W];
+		visited = new boolean[H][W][K+1];
+		
+		for(int i=0; i<H; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0; j<W; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}// 입력완료
+		
+//		printMap();
+		
+		int result = bfs();
+		System.out.println(result);
+		
+	}
+	
 
-    static int[][] Horse = {
-            {-2, 1},  //1
-            {-1, 2},  //2
-            {1, 2},   //3
-            {2, 1},   //4
-            {2, -1},  //5
-            {1, -2},  //6
-            {-1, -2}, //7
-            {-2, -1}, //8
-    };
+	static int bfs() {
+		Queue<int[]> q = new ArrayDeque<>();
+		q.add(new int[] {0,0, K,0});
+		visited[0][0][K] = true;
+		
+		while(!q.isEmpty()) {
+			int[] cur = q.poll();
+			int x = cur[0];
+			int y = cur[1];
+			int k = cur[2];
+			int dep = cur[3];
+			
+			if(x == H-1 && y == W-1) return dep;
+			
+			// 1.상		2.하		3.좌		4.우		5.말
+			for(int i=0; i<4; i++) { // 원숭이 상 하 좌 우
+				int nx = x + D[i][0];
+				int ny = y + D[i][1];
+				
+				if(nx < 0 || ny < 0 || nx >= H || ny >= W) continue;
+				if(visited[nx][ny][k]) continue;
+				if(map[nx][ny] == 1) continue;
+				
+				visited[nx][ny][k] = true;
+				q.add(new int[] {nx,ny,k,dep+1});
+			}
+			if(k > 0) { // 말 이동 8가지
+				for(int i=0; i<8; i++) {
+					int nx = x + Horse[i][0];
+					int ny = y + Horse[i][1];
+					
+					if(nx < 0 || ny < 0 || nx >= H || ny >= W) continue;
+					if(visited[nx][ny][k-1]) continue;
+					if(map[nx][ny] == 1) continue;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        K = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        W = Integer.parseInt(st.nextToken());
-        H = Integer.parseInt(st.nextToken());
-
-        map = new int[H][W];
-        visited = new boolean[H][W][K + 1]; // K + 1 만큼의 차원 추가
-
-        for (int i = 0; i < H; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < W; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-            }
-        } // 입력 완료
-        
-        //출력
-//        for(int[] e : map) System.out.println(Arrays.toString(e));
-        
-        System.out.println(bfs());
-    }
-
-    static int bfs() {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{0, 0, 0, K}); // (x,y)좌표, cnt, K
-        visited[0][0][K] = true;
-
-        while (!queue.isEmpty()) {
-            int[] cur = queue.poll();
-            int x = cur[0];
-            int y = cur[1];
-            int cnt = cur[2]; // 이동 횟수
-            int k = cur[3];
-
-            if (x == H - 1 && y == W - 1) {
-                return cnt; // 이동 횟수 반환
-            }
-
-            for (int[] d : D) {
-                int nx = x + d[0];
-                int ny = y + d[1];
-
-                if (0 <= nx && nx < H && 0 <= ny && ny < W && !visited[nx][ny][k] && map[nx][ny] == 0) {
-                    visited[nx][ny][k] = true; // 방문 처리
-                    queue.add(new int[]{nx, ny, cnt + 1, k});
-                }
-            }
-
-            // 말처럼 이동
-            if (k > 0) {
-                for (int[] HM : Horse) {
-                    int nx = x + HM[0];
-                    int ny = y + HM[1];
-
-                    if (0 <= nx && nx < H && 0 <= ny && ny < W && !visited[nx][ny][k - 1] && map[nx][ny] == 0) {
-                        visited[nx][ny][k - 1] = true; // 방문 처리
-                        queue.add(new int[]{nx, ny, cnt + 1, k - 1}); // 큐에 추가
-                    }
-                }
-            }
-        }
-
-        return -1; // 도달할 수 없는 경우
-    }
+					visited[nx][ny][k-1] = true;
+					q.add(new int[] {nx,ny,k-1,dep+1});
+				}
+			}
+		}
+		return -1;
+	}
+	
+	static void printMap() {
+		for(int[] e : map) System.out.println(Arrays.toString(e));
+	}
+	
 }
