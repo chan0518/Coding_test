b71050b53e965f764c6843baa514403f019a8e61
Chan_E <114585572+chan0518@users.noreply.github.com>
2024-11-19 17:35:27 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 384 ms, Memory: 40308 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
@@ -1,53 +1,63 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int n, max;
-    static int[][] map, dp;
-    static int[][] D = {
-        {-1, 0},    // 상
-        {1, 0},     // 하
-        {0, -1},    // 좌
-        {0, 1},     // 우
-    };
-    
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        n = Integer.parseInt(br.readLine());
-        map = new int[n][n];
-        dp = new int[n][n];
-        
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }// 입력 완료
-        
-        max = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                max = Math.max(max, dfs(i, j));
-            }
-        }
-        System.out.println(max);
-    }
-    
-    static int dfs(int x, int y) {
-        if (dp[x][y] != 0) return dp[x][y]; // 이미 계산된 경우
-        dp[x][y] = 1;
-        
-        for(int[] d : D) {
-        	int nx = x + d[0];
-        	int ny = y + d[1];
-        	
-        	if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-        	
-        	if (map[x][y] < map[nx][ny]) {
-                dp[x][y] = Math.max(dp[x][y], dfs(nx, ny) + 1);
-            }
-        }
-        return dp[x][y];
-    }
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static int N;
+	static int[][] map;
+	static int[][] dp;
+	static int[][] D = {
+			{-1, 0},	// 상
+			{1, 0},		// 하
+			{0, -1},	// 좌
+			{0, 1},		// 우
+	};
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		map = new int[N][N];
+		dp = new int[N][N];
+		
+		for(int i=0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0; j<N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}// 입력완료
+		
+		
+		int max = 0;
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<N; j++) {
+				max = Math.max(max, dfs(i,j));
+			}
+		}
+		System.out.println(max);
+		
+		//for(int[] e1: dp) System.out.println(Arrays.toString(e1));
+	}	
+	
+	static int dfs(int x, int y) {
+		if(dp[x][y] != 0) return dp[x][y];
+		dp[x][y] = 1;
+		
+		for(int i=0; i<4; i++) {
+			int nx = x + D[i][0];
+			int ny = y + D[i][1];
+			
+			if(nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+			if(map[x][y] < map[nx][ny]) dp[x][y] = Math.max(dp[x][y], dfs(nx,ny) + 1);
+//			if(map[x][y] < map[nx][ny]) dp[x][y] = dfs(nx,ny) + 1;
+			
+		}
+		return dp[x][y];
+	}
 }
