c851c7b59c1c765320131c34b143c1b47bde79ec
Chan_E <114585572+chan0518@users.noreply.github.com>
2024-11-19 21:53:03 +0900

    [Gold III] Title: 내리막 길, Time: 268 ms, Memory: 39936 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -1,57 +1,63 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n,m;
-    static int[][] board;
-    static int[][] dp;
-    static int[][] D = {	{1,0},
-    							{-1,0},
-    							{0,1},
-    							{0,-1}	};
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static int N, M;
+	static int[][] map;
+	static int[][] memo;
+	static int[][] D = {
+			{-1, 0},	// 상
+			{1, 0},		// 하
+			{0, -1},	// 좌
+			{0, 1},		// 우
+	};
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		map = new int[N][M];
+		memo = new int[N][M];
+		
+		for(int i=0; i<N; i++) Arrays.fill(memo[i], -1);
+		
+		for(int i=0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0; j<M; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}// 입력완료
+		
+//		for(int[] e : map) System.out.println(Arrays.toString(e));
+		dfs(0,0);
+//		for(int[] e : memo) System.out.println(Arrays.toString(e));
 
-    static void dfs(int y, int x){
-        if(y== m-1 & x==n-1){
-            dp[y][x] = 1;
-            return;
-        }
-        if(dp[y][x] != -1)
-            return;
-        
-        dp[y][x] = 0;
-        for(int[] d: D){
-            int next_x = x+d[1];
-            int next_y = y+d[0];
-
-            if(next_x < n & next_x>=0 & next_y < m & next_y>=0){
-                if(board[y][x] > board[next_y][next_x]){
-                    dfs(next_y, next_x);
-                    dp[y][x] += dp[next_y][next_x];
-                }
-            }
-        }
-    }
-    public static void main(String[] args) throws IOException{
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	StringTokenizer st = new StringTokenizer(br.readLine());
-        m = Integer.valueOf(st.nextToken());
-        n = Integer.valueOf(st.nextToken());
-        board = new int[m][n];
-        dp = new int[m][n];
-
-        for(int i=0; i<m;i++) {
-        	st = new StringTokenizer(br.readLine());
-            for(int j=0; j<n; j++){
-                board[i][j] = Integer.valueOf(st.nextToken());
-                dp[i][j] = -1;
-            }
-        }
-
-        dfs(0, 0);
-
-        System.out.println(dp[0][0]);
-    }
-    
+		System.out.println(memo[0][0]);
+	}
+	
+	static int dfs(int x, int y) {
+		if(memo[x][y] != -1) return memo[x][y]; // 이미 계산된 값이라면
+		if(x == N-1 && y == M-1) return 1;
+		memo[x][y] = 0;
+		
+		for(int i=0; i<4; i++) {
+			int nx = x + D[i][0];
+			int ny = y + D[i][1];
+			
+			if(nx < 0 || ny < 0 || nx >= N || ny >= M) continue; // 범위 벗어나면
+			if(map[x][y] <= map[nx][ny]) continue; // 다음값이 작지 않다면
+			
+			memo[x][y] += dfs(nx,ny);
+		}
+		return memo[x][y];
+	}
+	
+	
 }
